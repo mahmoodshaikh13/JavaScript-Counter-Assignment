281ec50e535a40e0036d793a408303a1e79db71a
mahmoodshaikh13 <156595532+mahmoodshaikh13@users.noreply.github.com>
2024-02-18 18:57:59 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ const btnDecrement = document.querySelector('#decrement');
 const btnReset = document.querySelector('#reset');
 const btnIncrement = document.querySelector('#increment');
 
-btnIncrement.addEventListener('click', () => {
-  curValue++;
-  mainTitle.textContent = curValue;
-});
 btnDecrement.addEventListener('click', () => {
     curValue--;
     mainTitle.textContent = curValue;
